bb56055f1bfd305790f0dad3be401d76aa941ed8
Ritik Jagota <103742459+ritik4475@users.noreply.github.com>
2024-07-25 14:22:23 +0530

    upload

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { router } from "expo-router";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
+
+
+export default function HomeScreen() {
+
+  return (
+    <View>
+      <Text>Your Album</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
